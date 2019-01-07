669bc5fdf2c8e7472f4e26800d02c3d2074fb2f1
Levi Meahan <lsmjudoka@worldofbleach-rpg.com>
2019-01-06 19:39:50 -0800

    Add template for react components

---
+++ .idea/fileTemplates/React Component.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite/no-important';
+
+const ${NAME} = () => {
+    return <div>${NAME}</div>;
+};
+
+const styles = StyleSheet.create({
+    
+});
+
+export default ${NAME};
