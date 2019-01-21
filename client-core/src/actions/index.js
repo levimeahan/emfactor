c3fc1806b82fa30dc9bfdd2e7c59cd6437f29f5f
Levi Meahan <lsmjudoka@worldofbleach-rpg.com>
2019-01-21 08:46:59 -0800

    remove unneeded compiled js files

--- client-core/src/actions/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var login_1 = require("./login");
-exports.login = login_1["default"];
-var logout_1 = require("./logout");
-exports.logout = logout_1["default"];
